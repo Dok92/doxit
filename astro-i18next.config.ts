2ec8dad951d48cb63ddd7f8920ee6faa05369b8c
Dok92 <doslovdok@gmail.com>
2025-10-17 20:33:15 +0200

    Updated index

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -1,6 +1,16 @@
 /** @type {import('astro-i18next').AstroI18nextConfig} */
 
 export default {
-  defaultLocale: "de",
-  locales: ["de", "en"]
+  defaultLocale: 'de',
+  locales: ['de', 'en'],
+  routes: {
+    de: {
+      rent: 'mieten',
+      transport: 'transportieren',
+    },
+    en: {
+      rent: 'rent',
+      transport: 'transport',
+    },
+  },
 };
